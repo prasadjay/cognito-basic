f4e1f1b5843586992b635e233cdc0c222084caaf
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-03-09 00:47:39 +0530

    added first codes

---
+++ app.js
@@ -0,0 +1,107 @@
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+var AWS = require('aws-sdk');
+var request = require('request');
+var jwkToPem = require('jwk-to-pem');
+var jwt = require('jsonwebtoken');
+
+const poolData = {
+    UserPoolId : "", // Your user pool id here
+    ClientId : "" // Your client id here
+};
+
+const pool_region = 'us-east-1';
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function RegisterUser(){
+    var attributeList = [];
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:"Prasad Jayashanka"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"preferred_username",Value:"jay"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"gender",Value:"male"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"birthdate",Value:"1991-06-21"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"address",Value:"CMB"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:"sampleEmail@gmail.com"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"phone_number",Value:"+5412614324321"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"custom:role",Value:"admin"}));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"custom:tenant",Value:"cookiemonster"}));
+
+    userPool.signUp('sampleEmail@gmail.com', 'SamplePassword123', attributeList, null, function(err, result){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+    });
+}
+
+function Login() {
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username : 'sampleEmail@gmail.com',
+        Password : 'SamplePassword123',
+    });
+
+    var userData = {
+        Username : 'sampleEmail@gmail.com',
+        Pool : userPool
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            console.log('access token + ' + result.getAccessToken().getJwtToken());
+            console.log('id token + ' + result.getIdToken().getJwtToken());
+            console.log('refresh token + ' + result.getRefreshToken().getToken());
+        },
+        onFailure: function(err) {
+            console.log(err);
+        },
+
+    });
+}
+
+function ValidateToken(token) {
+        request({
+            url: `https://cognito-idp.${pool_region}.amazonaws.com/${poolData.UserPoolId}/.well-known/jwks.json`,
+            json: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                pems = {};
+                var keys = body['keys'];
+                for(var i = 0; i < keys.length; i++) {
+                    //Convert each key to PEM
+                    var key_id = keys[i].kid;
+                    var modulus = keys[i].n;
+                    var exponent = keys[i].e;
+                    var key_type = keys[i].kty;
+                    var jwk = { kty: key_type, n: modulus, e: exponent};
+                    var pem = jwkToPem(jwk);
+                    pems[key_id] = pem;
+                }
+                //validate the token
+                var decodedJwt = jwt.decode(token, {complete: true});
+                if (!decodedJwt) {
+                    console.log("Not a valid JWT token");
+                    return;
+                }
+
+                var kid = decodedJwt.header.kid;
+                var pem = pems[kid];
+                if (!pem) {
+                    console.log('Invalid token');
+                    return;
+                }
+
+                jwt.verify(token, pem, function(err, payload) {
+                    if(err) {
+                        console.log("Invalid Token.");
+                    } else {
+                        console.log("Valid Token.");
+                        console.log(payload);
+                    }
+                });
+            } else {
+                console.log("Error! Unable to download JWKs");
+            }
+        });
+}
